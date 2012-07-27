e5a7dc6ff1084d5bc0fb4b78f3b22e5c8033253c
Rémi <remi.chaignon@gmail.com>
2012-07-27 17:50:37 -0600

    RendererBB as a library

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-renderer-bb
-===========
+# renderer-bb
 
 Javascript WebGL library based on Backbone.
+
+
+## Status
+
+In Progress - Built for [photoworld_web](https://github.com/baguetteapps/photoworld_web).
+
+
+## Dependencies
+
+- [jQuery](http://jquery.com/)
+- [Underscore](http://underscorejs.org/)
+- [Backbone](http://backbonejs.org/)
+
+
+## Example
+
+1. Clone this repository
+2. Install node.js
+3. Launch the local server - node server.js
+4. Compile shaders - python shaders.py
+4. Compile objects - python objects.py
+5. Go to [http://127.0.0.1:1337](http://127.0.0.1:1337)
+6. Browse the pages and change language
+
+
+## Usage
+
+0. Add renderer-bb.js to your scripts,
+1. Compile your shaders using the python script (python shaders.py), change the path to your shaders in the script if necessary,
+2. Compile your objects using the python script (python objects.py), change the path to your objects in the script if necessary,
+3. ...
+4. Finally, in your app, add this line:
+```JS
+	RendererBB.ready(my_renderer_holder, my_options, my_callback);
+```
+Where my_renderer_holder is where your renderer will be stored; my_options is parameters you wish to pass to the renderer (like the baseURL to your shaders and objects folder, etc); my_callback is a function to be executed once the renderer is ready.
+
+
+## License
+
+renderer-bb.js is released under the MIT license.
+
+
+## Author
+
+[Rémi Chaignon](http://www.github.com/remichaignon) - Front End Developer at Simple Energy - [@remichaignon](http://twitter.com/remichaignon)
---
+++ app/app.js
@@ -0,0 +1,77 @@
+//
+// app.js
+//
+// Example application for renderer-bb.
+//
+// ============================================================================
+//
+// APPLICATION
+// INITIALIZATION
+//
+// ============================================================================
+//
+
+
+(function(window, $) {
+
+	// Global configuration
+	var config = {
+		url: "http://127.0.0.1:1337"
+	};
+
+	// Test classes holder
+	app = {};
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// $APPLICATION
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// $INITIALIZATION
+
+	app.init = function(){
+		console.log("Hello app using renderer-bb!");
+
+		var renderObjects = function() {
+			var gl = app.renderer.view.gl;
+
+			// Projection matrix
+			var pMatrix = mat4.create();
+			mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+
+			// Get objects
+			var triangle = app.renderer.get("objects").where({ name: "triangle" })[0];
+			var square = app.renderer.get("objects").where({ name: "square" })[0];
+
+			// Triangle's ModelView matrix
+			var values = triangle.get("values");
+			var mvMatrix = mat4.create();
+			mat4.identity(mvMatrix);
+			mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
+			values["uPMatrix"] = pMatrix;
+			values["uMVMatrix"] = mvMatrix;
+			triangle.set("values", values);
+
+			// Square's ModelView matrix
+			values = square.get("values");
+			var mvMatrix2 = mat4.create();
+			mat4.identity(mvMatrix2);
+			mat4.translate(mvMatrix2, [1.5, 0.0, -7.0]);
+			values["uPMatrix"] = pMatrix;
+			values["uMVMatrix"] = mvMatrix2;
+			square.set("values", values);
+
+			// Render all
+			app.renderer.view.render();
+		};
+
+		// Build page one once the renderer is ready
+		RendererBB.ready(app, { baseURL: config.url + "/app" }, renderObjects);
+	};
+
+
+	window.app = app;
+	$(document).ready(app.init);
+
+})(this, jQuery);
