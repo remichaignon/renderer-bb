d9da59065ee695c7d9e7a30fc013b91f55bd54f6
Rémi <remi.chaignon@gmail.com>
2012-08-16 16:49:53 -0600

    Make the objects rotate

--- app/app.js
+++ app/app.js
@@ -47,23 +47,37 @@
 			// Triangle's ModelView matrix
 			var values = triangle.get("values");
 			var mvMatrix = mat4.create();
-			mat4.identity(mvMatrix);
-			mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
-			values["uPMatrix"] = pMatrix;
-			values["uMVMatrix"] = mvMatrix;
-			triangle.set("values", values);
 
 			// Square's ModelView matrix
 			values = square.get("values");
 			var mvMatrix2 = mat4.create();
-			mat4.identity(mvMatrix2);
-			mat4.translate(mvMatrix2, [1.5, 0.0, -7.0]);
-			values["uPMatrix"] = pMatrix;
-			values["uMVMatrix"] = mvMatrix2;
-			square.set("values", values);
 
-			// Render all
-			app.renderer.view.render();
+			var rotation = 0;
+
+			setInterval(
+				function() {
+					mat4.identity(mvMatrix);
+					mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
+					mat4.rotate(mvMatrix, rotation * Math.PI / 180, [1, 0, 0]);
+					values = triangle.get("values");
+					values["uPMatrix"] = pMatrix;
+					values["uMVMatrix"] = mvMatrix;
+					triangle.set("values", values);
+
+					mat4.identity(mvMatrix2);
+					mat4.translate(mvMatrix2, [1.5, 0.0, -7.0]);
+					mat4.rotate(mvMatrix2, rotation * Math.PI / 180, [0, 1, 0]);
+					values2 = square.get("values");
+					values2["uPMatrix"] = pMatrix;
+					values2["uMVMatrix"] = mvMatrix2;
+					square.set("values", values2);
+
+					// Render all and update angle
+					app.renderer.view.render();
+					rotation++;
+				},
+				10
+			);
 		};
 
 		// Render all objects once the renderer is ready
